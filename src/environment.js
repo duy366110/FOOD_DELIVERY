e4254d01610e0a9c7b4846aa229d0a3455f2a089
duy366110 <duy366110@gmail.com>
2023-12-17 10:25:05 +0700

    #Commit 17/12/2023: Config origin host reques from admin page.

--- src/environment.js
+++ src/environment.js
@@ -2,7 +2,10 @@ const environment = {
     port: 8080,
     origin: {
         dev: ["http://localhost:4200"],
-        pro: []
+        pro: [
+            "https://fooddeliveryad-7a097.web.app",
+            "https://fooddeliveryad-7a097.firebaseapp.com"
+        ]
     }
 }
 
