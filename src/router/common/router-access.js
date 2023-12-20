9212d830cdf66e8990d37c80f5063512164a5d91
duy366110 <duy366110@gmail.com>
2023-12-20 16:30:20 +0700

    #Commit 20/12/2023: Config service user signout v-01.

--- src/router/common/router-access.js
+++ src/router/common/router-access.js
@@ -3,5 +3,6 @@ import controllerAccess from "../../controller/common/controller-access.js";
 const router = Router();
 
 router.post("/admin/signin", controllerAccess.adminSignin);
+router.post("/admin/signout", controllerAccess.adminSignout);
 
 export default router;
